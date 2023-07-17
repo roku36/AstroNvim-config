dec54abe73123640dcc89d0b485108e8da2c1ffc
roku36 <roku36000@gmail.com>
2023-07-17 21:56:02 +0900

    obsidian event

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,23 +1,21 @@
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
-  -- "nvim-lua/plenary.nvim",
+  "andweeb/presence.nvim",
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "BufRead",
+    config = function() require("lsp_signature").setup() end,
+  },
+  "nvim-lua/plenary.nvim",
   --
   -- -- Optional, for completion.
-  -- "hrsh7th/nvim-cmp",
+  "hrsh7th/nvim-cmp",
   --
   -- -- Optional, for search and quick-switch functionality.
-  -- "nvim-telescope/telescope.nvim",
+  "nvim-telescope/telescope.nvim",
   --
   -- -- Optional, alternative to nvim-treesitter for syntax highlighting.
-  -- "godlygeek/tabular",
-  -- "preservim/vim-markdown",
+  "godlygeek/tabular",
+  "preservim/vim-markdown",
 }
