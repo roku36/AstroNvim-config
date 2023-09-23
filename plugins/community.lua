7ccef82d226233878d09c9572e0794df4e7de481
roku36 <roku36000@gmail.com>
2023-09-24 04:55:10 +0900

    no transparent.nvim

--- plugins/community.lua
+++ plugins/community.lua
@@ -12,7 +12,7 @@ return {
   { import = "astrocommunity.pack.tailwindcss" },
   { import = "astrocommunity.file-explorer.oil-nvim" },
   { import = "astrocommunity.completion.codeium-vim" },
-  { import = "astrocommunity.utility.transparent-nvim" },
+  -- { import = "astrocommunity.utility.transparent-nvim" },
   { import = "astrocommunity.editing-support.chatgpt-nvim" },
   { import = "astrocommunity.motion.leap-nvim" },
   { import = "astrocommunity.editing-support.auto-save-nvim" },
